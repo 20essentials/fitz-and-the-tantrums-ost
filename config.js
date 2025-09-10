0e24acbd2a1d0294951fc81845bc709517fd070f
DominicDev <s0l0estudi4r@gmail.com>
2025-09-10 17:54:36 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FITZ AND THE TANTRUMS';
+export let nameSongs = [
+	"SPARK", /*1*/
+	"6AM", /*2*/
+	"A PLACE FOR US", /*3*/
+	"HANDCLAP", /*4*/
+	"THE WALKER", /*5*/
+	"OUT OF MY LEAGUE", /*6*/
+	"FOOLS GOLD", /*7*/
+	"123456", /*8*/
+	"MONEYGRABBER", /*9*/
+	"ALL THE FEELS", /*10*/
+	"I JUST WANNA SHINE", /*11*/
+	"BURN IT DOWN", /*12*/
+	"ROLL UP", /*13*/
+	"BREAK THE WALLS", /*14*/
+	"SWAY", /*15*/
+	"HEAVEN", /*16*/
+	"GET RIGHT BACK", /*17*/
+	"STOP", /*18*/
+	"EDGE OF THE EARTH", /*19*/
+	"ECHELON", /*20*/
+]
